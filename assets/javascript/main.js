04aa80bbae3696af3a67a1fc1112e6ef093e6fc9
Renan Pimentel <renan.pimentel@gmail.com>
2015-02-05 18:38:25 -0200

    Adicionando geolocalização para ver pontos mais próximos

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -6,6 +6,34 @@ function initialize() {
       mapTypeId: 'terrain'
   };
   var image = 'https://raw.githubusercontent.com/renanmpimentel/tccc-map/gh-pages/assets/images/bus.png';
+  var imageGeolocation = 'https://raw.githubusercontent.com/renanmpimentel/tccc-map/gh-pages/assets/images/geolocation.png';
+
+  // Try HTML5 geolocation
+  if(navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = new google.maps.LatLng(position.coords.latitude,
+                                       position.coords.longitude);
+
+      var geolocationWindow = new google.maps.InfoWindow({
+        map: map,
+        position: pos,
+        content: 'Você está aqui!'
+      });
+
+      var geolocationMarker = new google.maps.Marker({
+          position: pos,
+          map: map,
+          icon: imageGeolocation
+      });
+
+      map.setCenter(pos);
+    }, function() {
+      handleNoGeolocation(true);
+    });
+  } else {
+    // Browser doesn't support Geolocation
+    handleNoGeolocation(false);
+  }
 
   map = new google.maps.Map(document.getElementById('map-canvas'), myOptions);
 
@@ -138,4 +166,23 @@ addresses.forEach(function(each) {
 
 }
 
+
+function handleNoGeolocation(errorFlag) {
+  if (errorFlag) {
+    var content = 'Error: The Geolocation service failed.';
+  } else {
+    var content = 'Error: Your browser doesn\'t support geolocation.';
+  }
+
+  var options = {
+    map: map,
+    position: new google.maps.LatLng(60, 105),
+    content: content
+  };
+
+  var geolocationWindow = new google.maps.InfoWindow(options);
+  map.setCenter(options.position);
+}
+
 google.maps.event.addDomListener(window, 'load', initialize);
+
